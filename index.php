c2c39b24a2adb0509f68af1d5978e471f2acbe5d
laerte-guimaraes <laerte.guimaraes@vindi.com.br>
2018-08-15 17:01:02 -0300

    Commit Inicial

---
+++ index.php
@@ -0,0 +1,168 @@
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<title>Vindi Integrações</title>
+		<base href="http://localhost">
+		<meta name="viewport" content="width=1200">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<!-- Facebook Open Graph -->
+		<meta name="og:title" content="Vindi Integrações">
+		<meta name="og:description" content="">
+		<meta name="og:image" content="">
+		<meta name="og:type" content="article">
+		<meta name="og:url" content="localhost">
+		<!-- Facebook Open Graph end -->
+		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+		<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+		<script src="js/bootstrap.min.js" type="text/javascript"></script>
+		<script src="js/main.js?v=20180718102713" type="text/javascript"></script>
+		<link href="css/font-awesome/font-awesome.min.css?v=4.7.0" rel="stylesheet" type="text/css">
+		<link href="css/site.css?v=20180718102714" rel="stylesheet" type="text/css">
+		<link href="css/common.css?ts=1532010081" rel="stylesheet" type="text/css">
+		<link href="css/1.css?ts=1532010081" rel="stylesheet" type="text/css">
+		<script type="text/javascript">var currLang = '';</script>
+		<!--[if lt IE 9]>
+		<script src="js/html5shiv.min.js"></script>
+		<![endif]-->
+	</head>
+
+
+	<body style="height: 762px; min-height: 762px;">
+		<div class="root" style="height: 762px; min-height: 762px;">
+			<div class="vbox wb_container" id="wb_main" style="height: 705px;">
+				<div class="wb_cont_inner">
+
+					<!-- Título ou logo Vindi -->
+					<div id="wb_element_instance9" class="wb_element" style=" line-height: normal;">
+						<h1 class="wb-stl-heading1">
+							<span class="wb-stl-special">
+								<span class="wb_tr_ok">Vindi</span>
+							</span>
+						</h1>
+					</div>
+
+					<!-- Subtítulo do site "Integrações" -->
+					<div id="wb_element_instance1" class="wb_element" style=" line-height: normal;">
+						<h4 class="wb-stl-pagetitle">
+							<span style="color:rgba(255,255,255,1);">
+								<strong>Integrações</strong>
+							</span>
+						</h4>
+					</div>
+					
+					<!-- Início Destaque -->
+						<!-- Imagem de Fundo -->
+							<div id="wb_element_instance10" class="wb_element wb_element_picture" title="">
+								<img alt="gallery/discount" src="gallery_gen/ec711503c996323395ee91247a40f9a3_270x180.png">
+							</div>
+						<!-- Texto -->
+							<div id="wb_element_instance11" class="wb_element" style=" line-height: normal;">
+								<h4 class="wb-stl-pagetitle" style="text-align: center;">
+									<strong>
+										<span style="color:rgba(255,255,255,1);">popular :)</span>
+									</strong>
+								</h4>
+							</div>
+							<div id="wb_element_instance12" class="wb_element" style=" line-height: normal;">
+								<h4 class="wb-stl-pagetitle" style="text-align: center;">
+									<strong>
+										<span style="color:rgba(255,255,255,1);">Mais </span>
+									</strong>
+								</h4>
+							</div>				
+					<!-- Final Destaque -->
+
+					<!-- Início Magento 1 -->
+						<!-- Box -->
+							<div id="wb_element_instance4" class="wb_element wb_element_shape">
+								<div class="wb_shp"></div>
+							</div>
+						<!-- Logo -->
+							<div id="wb_element_instance13" class="wb_element wb_element_picture" title="">
+								<img alt="gallery/magento" src="gallery_gen/8cca3a4115ce307f2518a97c6f2eace5_250x80.png">
+							</div>
+						<!-- Texto -->
+							<div id="wb_element_instance3" class="wb_element" style=" line-height: normal;">
+								<p style="text-align: justify;">Módulo da Vindi para Magento 1, com opção de trabalhar com compras simples e recorrentes, incluindo pagamentos através de cartão de crédito, débito e boleto bancário.</p>
+							</div>
+						<!-- Botão -->
+							<div id="wb_element_instance6" class="wb_element">
+								<a class="wb_button" href="http://vindi.store/magento" title="Magento">
+									<span>Visualizar</span>
+								</a>
+							</div>
+					<!-- Final Magento 1 -->
+
+					<!-- Início Magento 2 -->
+						<!-- Box -->
+						<!-- Logo -->
+						<!-- Texto -->
+						<!-- Botão -->
+					<!-- Final Magento 2 -->
+
+					<!-- Início WooCommerce -->
+						<!-- Box -->
+							<div id="wb_element_instance17" class="wb_element wb_element_shape">
+								<div class="wb_shp"></div>
+							</div>
+						<!-- Logo -->
+							<div id="wb_element_instance14" class="wb_element wb_element_picture" title="">
+								<img alt="gallery/woocommerce" src="gallery_gen/65e9a8ffc3c0d0c91f34d2042345a1b2_260x80.png">
+							</div>
+						<!-- Texto -->
+							<div id="wb_element_instance7" class="wb_element" style=" line-height: normal;">
+								<p style="text-align: justify;">Plugin da Vindi para WooCommerce, onde é possível gerenciar assinaturas e compras simples com cartão de crédito e boleto bancário no próprio painel de controle.</p>
+							</div>
+						<!-- Botão -->
+							<div id="wb_element_instance2" class="wb_element">
+								<a class="wb_button" href="http://vindi.store/wordpress/loja" title="Wordpress">
+									<span>Visualizar</span>
+								</a>
+							</div>
+					<!-- Final WooCommerce -->
+
+					<!-- Início Opencart -->
+						<!-- Box -->
+							<div id="wb_element_instance15" class="wb_element wb_element_shape">
+								<div class="wb_shp"></div>
+							</div>
+						<!-- Logo -->
+							<div id="wb_element_instance16" class="wb_element wb_element_picture" title="">
+								<img alt="gallery/opencart" src="gallery_gen/e01f1ac04b35fe357bc1dc98f18af4a0_250x60.png">
+							</div>
+						<!-- Texto -->
+							<div id="wb_element_instance8" class="wb_element" style=" line-height: normal;">
+								<p style="text-align: justify;">Extensão da Vindi para Opencart, que disponibiliza a opção de vendas simples com o método cartão de crédito.</p>
+							</div>
+						<!-- Botão -->
+							<div id="wb_element_instance5" class="wb_element">
+								<a class="wb_button" href="http://vindi.store/opencart" title="Opencart">
+									<span>Visualizar</span>
+								</a>
+							</div>
+					<!-- Fim Opencart -->
+
+					<!-- Início Vtex -->
+						<!-- Box -->
+						<!-- Logo -->
+						<!-- Texto -->
+						<!-- Botão -->
+					<!-- Final Vtex -->
+
+					<div id="wb_element_instance18" class="wb_element" style="width: 100%; display: none;">
+						<script type="text/javascript">
+							$(function() {
+								$("#wb_element_instance18").hide();
+							});
+						</script>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<p style="text-align: center;">
+			<span style="color:rgba(255,255,255,1);">Vindi 2018 - Todos os direitos reservados - Made with love in Sao Paulo</span>
+		</p>
+	</body>
+</html>
